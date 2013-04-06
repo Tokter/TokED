024bec690eb815d0b231f52abd58c215c3426fc2
Markus Luedin <markus@luedin.com>
2013-04-06 15:48:27 -0400

    Added new tool "Toggle Select"

---
+++ PluginBase/Tools/SelectToggle.cs
@@ -0,0 +1,39 @@
+﻿using OpenTK;
+using OpenTK.Input;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Composition;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TokED;
+using TokGL;
+
+namespace PluginBase.Tools
+{
+    [Export("Select Toggle", typeof(EditorTool)), PartCreationPolicy(CreationPolicy.Shared)]
+    public class SelectToggle : EditorTool
+    {
+        public SelectToggle()
+            : base(ToolEvent.CreateDown(MouseButton.Right, Modifier.Shift), false)
+        {
+        }
+
+        public override void Activated()
+        {
+            if (Editor.MouseOverControl.Selected)
+            {
+                Editor.UnSelect(Editor.MouseOverControl);
+            }
+            else
+            {
+                Editor.Select(Editor.MouseOverControl);
+            }
+        }
+
+        public override bool Availbable()
+        {
+            return Editor != null && Editor.Camera != null && Editor.MouseOverControl != null;
+        }
+    }
+}
